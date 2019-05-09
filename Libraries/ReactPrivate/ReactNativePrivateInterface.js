5fc65c787f9bcceee15640330b2510c7fbb4d81b
James Ide <ide@expo.io>
2019-05-09 10:46:56 -0700

    [standards] ReactPrivate, an explicit interface between the renderer and RN
    This introduces a new library named "ReactPrivate" that defines an explicit interface between the React renderers generated by the React repo and the code within RN. Previously, the React renderers would reach into RN internals via Haste wormholes. With this commit, there is now an explicit module (`ReactNativePrivateInterface`) that the renderers use to access RN internals.
    
    Motivation: The main goal is to move one step closer to turning off Haste for RN (https://github.com/facebook/react-native/issues/24316). Since the generated renderers currently use Haste, this commit sets the foundation for giving them a path-based interface to access RN internals.
    
    Additionally, this approach inverts abstraction control since RN needs to intentionally export its internals via the private interface instead of React reaching in via Haste.
    
    There will also need to be a corresponding commit to the React repo to make the renderers use this new interface. This RN commit needs to land before the React commit.
    
    Test Plan: Run unit tests, CI. This commit should be safe since it just introduces new modules.
    
    Also tested with newly generated renderers (not in this commit; needs to happen in the React repo) that use ReactPrivate instead of Haste and verified that RNTester loads and that unit tests pass.

---
+++ Libraries/ReactPrivate/ReactNativePrivateInterface.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow strict-local
+ */
+
+module.exports = {
+  get BatchedBridge() {
+    return require('./BatchedBridge/BatchedBridge.js');
+  },
+  get ExceptionsManager() {
+    return require('../Core/ExceptionsManager');
+  },
+  get FabricUIManager() {
+    return require('../ReactNative/FabricUIManager');
+  },
+  get Platform() {
+    return require('../Utilities/Platform');
+  },
+  get RCTEventEmitter() {
+    return require('../EventEmitter/RCTEventEmitter');
+  },
+  get ReactNativeViewConfigRegistry() {
+    return require('../Renderer/shims/ReactNativeViewConfigRegistry');
+  },
+  get TextInputState() {
+    return require('../Components/TextInput/TextInputState');
+  },
+  get UIManager() {
+    return require('../ReactNative/UIManager');
+  },
+  get deepDiffer() {
+    return require('../Utilities/differ/deepDiffer');
+  },
+  get deepFreezeAndThrowOnMutationInDev() {
+    return require('../Utilities/deepFreezeAndThrowOnMutationInDev');
+  },
+  get flattenStyle() {
+    return require('../StyleSheet/flattenStyle');
+  },
+};
